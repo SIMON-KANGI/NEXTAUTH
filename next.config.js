2fc098950def6ee52c9a66041521d10c23dc4b1b
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-28 19:18:40 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['group-c.onrender.com', 'res.cloudinary.com', 'media.post.rvohealth.io', 'www.universal-translation-services.com'], // Add your required domains here
+    },
+  };
+  
+  module.exports = nextConfig;
+  
